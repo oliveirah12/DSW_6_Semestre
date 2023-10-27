5187b85c58218249258911c39b4ad809485faa5c
oliveirah12 <89754992+oliveirah12@users.noreply.github.com>
2023-10-27 20:01:02 -0300

    Delete app-backend directory

--- app-backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app.js");
-    require("dotenv").config();
-
-    const port = process.env.SERVER_PORT || 8080;
-
-    app.listen(port, () =>{
-        console.log(`Server listening on port ${port}`);
-    });
