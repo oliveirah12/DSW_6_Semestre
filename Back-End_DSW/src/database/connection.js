589c1bef64045fd4e456007801210d821a7d570b
estacionamento <cesarvatec@hotmail.com>
2023-11-14 13:05:56 -0300

    tabela de relatorios

--- Back-End_DSW/src/database/connection.js
+++ Back-End_DSW/src/database/connection.js
@@ -3,7 +3,7 @@ var knex = require('knex')({
     connection: { 
         host: 'localhost',   
         user: 'root',  
-        password : 'mat2101',       
+        password : '',       
         database : 'estufa'    
     }
 }); 
