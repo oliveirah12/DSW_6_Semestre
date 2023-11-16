0a5a628eb835afefea1fa206d3ac958307616871
estacionamento <cesarvatec@hotmail.com>
2023-11-16 10:55:54 -0300

    mudança_de_nome

--- Back-End_DSW/src/routes/routes.js
+++ Back-End_DSW/src/routes/routes.js
@@ -1,8 +1,10 @@
-const connection = require('../database/connection')
+const connection = require('../database/knexConfig')
 const express = require('express')
 const router = express.Router()
 const ReportsController = require('../controllers/ReportsController')
 const controlesController = require('../controllers/controlesController')
+const StatusController = require('../controllers/StatusController');
+
 
 
 // Rotas para relatórios
