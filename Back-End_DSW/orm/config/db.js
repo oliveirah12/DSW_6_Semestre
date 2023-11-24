ad7a2445d133fb8050ead83de3d7a309f6b5aa78
oliveirah12 <89754992+oliveirah12@users.noreply.github.com>
2023-11-24 20:14:00 -0300

    Delete Back-End_DSW directory

--- Back-End_DSW/orm/config/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("crud-db", "user", "password", {
-  dialect: "sqlite",
-  host: "./dev.sqlite",
-});
-
-module.exports = sequelize;
